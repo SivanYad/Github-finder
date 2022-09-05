62e889d95aeed9c17dcd08dc36bf95e52447ddc4
unknown <justsivan@gmail.com>
2022-09-05 16:03:39 +0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import UserResults from '../componenets/users/UserResults'
+
+function Home() {
+  return (
+    <>
+        {/* {SEARCH COMPONENT} */}
+        <UserResults />
+        
+    </>
+  )
+}
+
+export default Home
