b1c38b67c4f0bfc96855e10e5feb5cd1c43adb56
unknown <justsivan@gmail.com>
2022-09-05 17:19:04 +0300

    started working on users search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import UserResults from '../componenets/users/UserResults'
+import UserSearch from '../componenets/users/UserSearch'
 
 function Home() {
   return (
     <>
-        {/* {SEARCH COMPONENT} */}
+        <UserSearch />
         <UserResults />
         
     </>
