7395a0da8ecbf829bc2efa318b40fb43125aabf5
unknown <justsivan@gmail.com>
2022-09-06 12:13:22 +0300

    connected the user page to github api

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,18 +9,19 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: false
-  }
+    user: {},
+    loading: false,
+  };
 
-  const [state, dispatch] = useReducer(githubReducer, initialState)
+  const [state, dispatch] = useReducer(githubReducer, initialState);
 
-//Get search results
+  //Get search results
   const searchUsers = async (text) => {
-    setLoading()
+    setLoading();
 
     const params = new URLSearchParams({
-      q: text
-    })
+      q: text,
+    });
 
     const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
@@ -30,31 +31,47 @@ export const GithubProvider = ({ children }) => {
     const { items } = await response.json();
     dispatch({
       type: 'GET_USERS',
-      payload: items
-    })
+      payload: items,
+    });
   };
-  //Clear Users 
-  const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
 
-  // const fetchUsers = async () => {
-  //   setLoading()
-  //   const response = await fetch(`${GITHUB_URL}/users`, {
-  //     headers: {
-  //       Authorization: `token ${GITHUB_TOKEN}`,
-  //     },
-  //   });
-  //   const data = await response.json();
-  //   dispatch({
-  //     type: 'GET_USERS',
-  //     payload: data
-  //   })
-  // };
+  //Get single user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+    if (response.status === 404) {
+      window.location = '/notfound'
+    } else {
+
+      const data = await response.json();
+      dispatch({
+        type: 'GET_USER',
+        payload: data,
+      });
+    }
+  };
+  //Clear Users
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
 
   //Set Loading
-  const setLoading = () => dispatch({type: 'SET_LOADING'})
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
   return (
-    <GithubContext.Provider value={{ users: state.users, loading: state.loading, searchUsers, clearUsers }}>
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        loading: state.loading,
+        user: state.user,
+        searchUsers,
+        clearUsers,
+        getUser
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
