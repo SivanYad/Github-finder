f138f314a5aed5cdf9ec275e64b4c81604845d44
unknown <justsivan@gmail.com>
2022-09-06 11:06:03 +0300

    finished the alert cmp

--- src/componenets/users/UserSearch.jsx
+++ src/componenets/users/UserSearch.jsx
@@ -1,27 +1,30 @@
 import React from 'react';
 import { useState, useContext } from 'react';
 import GithubContext from '../../context/github/GithubContext';
+import AlertContext from '../../context/alert/AlertContext';
 
 function UserSearch() {
   const [text, setText] = useState('');
 
   const { users, searchUsers, clearUsers } = useContext(GithubContext);
 
+  const { setAlert } = useContext(AlertContext);
+
   const handleChange = (e) => setText(e.target.value);
   const handleSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
-      alert('please enter something');
+      setAlert('please enter something', 'error');
     } else {
-      searchUsers(text)
+      searchUsers(text);
 
       setText('');
     }
   };
 
   const handleClear = (e) => {
-    clearUsers()
-  }
+    clearUsers();
+  };
 
   return (
     <div className='grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
@@ -48,7 +51,9 @@ function UserSearch() {
       </div>
       {users.length > 0 && (
         <div>
-          <button onClick={handleClear} className='btn btn-ghost btn-lg'>Clear</button>
+          <button onClick={handleClear} className='btn btn-ghost btn-lg'>
+            Clear
+          </button>
         </div>
       )}
     </div>
